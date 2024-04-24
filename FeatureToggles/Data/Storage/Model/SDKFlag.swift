9a83402d9d404b81f403d0afb8a824ed2d35aca0
a.nesterkina <a.nesterkina@trueengineering.ru>
2024-04-24 17:33:38 +0700

    Fix SDKFlag

--- FeatureToggles/Data/Storage/Model/SDKFlag.swift
+++ FeatureToggles/Data/Storage/Model/SDKFlag.swift
@@ -1,11 +1,26 @@
 import Foundation
+import SwiftUI
 
-public struct SDKFlag: Codable {
+public struct SDKFlag: Codable, Identifiable {
+    
+    // MARK: - Properties
+    
     public var name: String
     public var isEnabled: Bool
+    public var description: String?
+    public var group: String?
+    
+    public var id: String {
+        return name
+    }
+    
 }
 
 struct SDKFlagsWithHash {
+    
+    // MARK: - Properties
+    
     var flags: [SDKFlag]
     var hash: String
+    
 }
